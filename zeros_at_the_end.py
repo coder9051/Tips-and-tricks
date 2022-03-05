c2446420b8ef6c97e23c4751bd3b236a1ce68621
coder9051 <84278583+coder9051@users.noreply.github.com>
2022-03-05 17:13:24 +0530

    Update zeros_at_the_end.py

--- zeros_at_the_end.py
+++ zeros_at_the_end.py
@@ -1,19 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-                                       **Move all zeros present in an array to the end**
-
-Q. Given an integer array, move all zeros present on it to the end. The solution should maintain the relative order of items in the 
-array and should not use constant space.
-
-There can be many ways to solve this problem. Following is a simple and interesting way to solve this problem. 
-
-
 SOLUTION 1:
-Traverse the given array ‘arr’ from left to right. While traversing, maintain count of non-zero elements in array. Let the count be ‘count’. For every non-zero element arr[i], put the element at ‘arr[count]’ and increment ‘count’. After complete traversal, all non-zero elements have already been shifted to front end and ‘count’ is set as index of first 0. Now all we need to do is that run a loop which makes all elements zero from ‘count’ till end of the array.
-Below is the implementation of the above approach. 
-
-The time complexity of the this solution is O(n), where n is the size of the input. 
-Auxillary space is O(1)
 """
 
 def shiftAllZerosToEnd(arr):
@@ -36,10 +23,7 @@ shiftAllZerosToEnd(arr)
 print(arr)
 
 """
-SOLUTION 2: Coders trick
-First we count number of zeros then we iterate through array and remove all the zeros and append zeros at the end of array for every zeros removed.
-Time complexity of this solution is greater then O(n) but no extra space is required.
-
+SOLUTION 2: 
 """
 
 arr = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9]
@@ -51,8 +35,6 @@ print(arr)
 
 """
 SOLUTION 3: Mathematical Coder!
-Sort the array by setting key to bool and reverse is true. In python the value of true is 1 and false is 0. When there is any number except 0 it returns true and false for zero.
-Time complexity of this solution is greater then O(nlogn) but no extra space is required.
 """
 
 arr = [1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9]
